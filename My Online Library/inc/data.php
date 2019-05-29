3d9671ce286ada71fd56d6fee4318da6fd92ced9
GuanyuLin <49344274+GuanyuLin@users.noreply.github.com>
2019-05-29 18:58:06 -0400

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,172 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+ "title" => "The Sunken Moon",
+ "img" => "img/media/sunkenmoon.jpg",
+ "genre" => "Novel,Advanture,Fantacy",
+ "format" => "Paperback",
+ "year" => 2018,
+ "category" => "Books",
+ "authors" => [
+     "Shui Quan",
+ ],
+ "publisher" => "Spring Publishing Group",
+ "isbn" => '978-9869572538'
+];
+$catalog[102] = [
+ "title" => "Shounen Onmyouji",
+ "img" => "img/media/shounen.jpg",
+ "genre" => "Novel,Advanture,Fantacy",
+ "format" => "Paperback",
+ "year" => 2014,
+ "category" => "Books",
+ "authors" => [
+     "Mitsuru Yuki",
+ ],
+ "publisher" => "CROWN CULTURE CORPORATION",
+ "isbn" => '978-9573331025'
+];
+$catalog[103] = [
+ "title" => "The Chronicles of Narnia",
+ "img" => "img/media/narnia.jpg",
+ "genre" => "Novel,Advanture,Fantacy",
+ "format" => "Paperback",
+ "year" => 2009,
+ "category" => "Books",
+ "authors" => [
+     "C. S. Lewis",
+ ],
+ "publisher" => "Zondervan",
+ "isbn" => '978-0061715051'
+];
+$catalog[104] = [
+  "title" => "The Irregular At Magic High School",
+  "img" => "img/media/magichighschool.jpg",
+ "genre" => "Novel,Advanture,Magic",
+ "format" => "Paperback",
+ "year" => 2012,
+ "category" => "Books",
+ "authors" => [
+     "Tsutomu Sato",
+ ],
+ "publisher" => "KADOKAWA TAIWAN CORPORATION",
+ "isbn" => '978-9862876084'
+];
+
+//Movies
+$catalog[201] = [
+ "title" => "Your Name",
+ "img" => "img/media/yourname.jpg",
+ "genre" => "Animation,Drama,Fantasy",
+ "format" => "DVD",
+ "year" => 2016,
+ "category" => "Movies",
+ "director" => "Makoto Shinkai",
+ "writers" => [
+     "Makoto Shinkai",
+     "Clark Cheng",
+ ],
+ "stars" => [
+     "Ryunosuke Kamiki",
+     "Mone Kamishiraishi",
+     "Ryo Narita",
+ ]
+];
+$catalog[202] = [
+ "title" => "Tangled",
+ "img" => "img/media/tangled.jpg",
+ "genre" => "Animation, Adventure, Comedy",
+ "format" => "DVD",
+ "year" => 2010,
+ "category" => "Movies",
+ "director" => [
+      "Nathan Greno",
+      "Byron Howard",
+ ],
+ "writers" => [
+     "Dan Fogelman",
+     "Jacob Grimm"
+ ],
+ "stars" => [
+     "Mandy Moore",
+     "Zachary Levi",
+     "Donna Murphy",
+ ]
+];
+$catalog[203] = [
+ "title" => "Harry Potter",
+ "img" => "img/media/harrypotter.jpg",
+ "genre" => "Adventure, Family, Fantasy",
+ "format" => "DVD",
+ "year" => 2001,
+ "category" => "Movies",
+ "director" => "Chris Columbus",
+ "writers" => [
+     "J.K. Rowling",
+     "Steve Kloves",
+ ],
+ "stars" => [
+     "Daniel Radcliffe",
+     "Rupert Grint",
+     "Richard Harris",
+ ]
+];
+$catalog[204] = [
+ "title" => "Pitch Perfect",
+ "img" => "img/media/pitch.jpg",
+ "genre" => "Comedy, Music, Romance",
+ "format" => "DVD",
+ "year" => 2012,
+ "category" => "Movies",
+ "director" => "Jason Moore",
+ "writers" => [
+     "Kay Cannon",
+     "Mickey Rapkin",
+ ],
+ "stars" => [
+     "Anna Kendrick",
+     "Brittany Snow",
+     "Rebel Wilson",
+ ]
+];
+
+//Music
+$catalog[301] = [
+ "title" => "PAGE TWO",
+ "img" => "img/media/pagetwo.jpg",
+ "genre" => "Kpop",
+ "format" => "CD",
+ "year" => 2016,
+ "category" => "Music",
+ "artist" => "TWICE"
+];
+$catalog[302] = [
+ "title" => "JUST US",
+ "img" => "img/media/justus.jpg",
+ "genre" => "Kpop",
+ "format" => "CD",
+ "year" => 2014,
+ "category" => "Music",
+ "artist" => "JYJ"
+];
+$catalog[303] = [
+ "title" => "COLOR*IZ",
+ "img" => "img/media/coloriz.png",
+ "genre" => "Kpop",
+ "format" => "CD",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "IZ*ONE"
+];
+$catalog[304] = [
+ "title" => "I AM",
+ "img" => "img/media/iam.jpg",
+ "genre" => "Kpop",
+ "format" => "CD",
+ "year" => 2018,
+ "category" => "Music",
+ "artist" => "(G)i-dle"
+];
+
+?>
